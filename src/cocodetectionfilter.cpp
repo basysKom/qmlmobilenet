ca4e08aaaafb6f87e088a6d5866000b28b60c56a
Berthold Krevert <berthold.krevert@basyskom.com>
2020-09-23 12:18:09 +0200

    Add Tensorflow Lite

--- src/cocodetectionfilter.cpp
+++ src/cocodetectionfilter.cpp
@@ -1,5 +1,14 @@
 #include "cocodetectionfilter.h"
 
+#include <QDebug>
+#include <QDir>
+#include <QStandardPaths>
+
+Q_LOGGING_CATEGORY(objectdetector, "video.tensorflow.objectdetector")
+
+const auto PathToMachineLearningModels = QStringLiteral("Development/MachineLearning/Models");
+const auto CocoModelSSD = QStringLiteral("ssd_mobilenet_v1_1_metadata_1.tflite");
+
 CocoDetectionFilter::CocoDetectionFilter( QObject* parent )
     : QAbstractVideoFilter( parent )
 {
@@ -13,18 +22,58 @@ QVideoFilterRunnable* CocoDetectionFilter::createFilterRunnable()
 
 CocoDetectionFilterRunnable::CocoDetectionFilterRunnable()
 {
+    const auto modelFile = QDir(PathToMachineLearningModels).filePath(CocoModelSSD);
+    initializeModel(QStandardPaths::locate(QStandardPaths::HomeLocation, modelFile));
 }
+
+void CocoDetectionFilterRunnable::initializeModel(const QString& filename)
+{
+    qCInfo(objectdetector) << "Loading model ...";
+    m_model = tflite::FlatBufferModel::BuildFromFile(filename.toLocal8Bit());
+
+    if (m_model == nullptr) {
+        qCWarning(objectdetector) << "Could not load model";
+        return;
+    }
+
+    qCInfo(objectdetector) << "Building interpreter ...";
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder builder(*m_model, resolver);
+    builder(&m_interpreter);
+
+    if (m_interpreter == nullptr) {
+        qCWarning(objectdetector) << "Could not build interpreter ...";
+        return;
+    }
+
+    if (m_interpreter->AllocateTensors() != kTfLiteOk) {
+        qCWarning(objectdetector) << "Could not allocate tensors";
+        return;
+    }
+
+    qCInfo(objectdetector) << "Interpreter state:";
+    tflite::PrintInterpreterState(m_interpreter.get());
+
+}
+
 
 QVideoFrame CocoDetectionFilterRunnable::run( QVideoFrame *input, const QVideoSurfaceFormat &surfaceFormat, RunFlags flags )
 {
     Q_UNUSED( flags )
-     Q_UNUSED( surfaceFormat )
+    Q_UNUSED( surfaceFormat )
 
     if ( !input ) {
         return QVideoFrame();
     }
 
-    QImage image = input->image();
+    const QImage::Format imageFormat = QVideoFrame::imageFormatFromPixelFormat(input->pixelFormat());
+    input->map(QAbstractVideoBuffer::ReadWrite);
+    QImage image = QImage(input->bits(),
+                 input->width(),
+                 input->height(),
+                 input->bytesPerLine(),
+                 imageFormat).copy();
+
     int width = image.width();
     int height = image.height();
 
@@ -39,5 +88,6 @@ QVideoFrame CocoDetectionFilterRunnable::run( QVideoFrame *input, const QVideoSu
         }
     }
 
+    input->unmap();
     return image;
 }
--- src/cocodetectionfilter.h
+++ src/cocodetectionfilter.h
@@ -1,8 +1,16 @@
 #ifndef __COCO_DETECTION_FILTER__
 #define __COCO_DETECTION_FILTER__
 
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/optional_debug_tools.h"
+
+#include <QLoggingCategory>
 #include <QAbstractVideoFilter>
 #include <QVideoFilterRunnable>
+
+Q_DECLARE_LOGGING_CATEGORY(objectdetector)
 
 class CocoDetectionFilter : public QAbstractVideoFilter
 {
@@ -17,6 +25,11 @@ class CocoDetectionFilterRunnable : public QVideoFilterRunnable
 public:
     CocoDetectionFilterRunnable();
     QVideoFrame run( QVideoFrame *input, const QVideoSurfaceFormat &surfaceFormat, RunFlags flags ) override;
+
+private:
+    void initializeModel(const QString &filename);
+    std::unique_ptr<tflite::FlatBufferModel> m_model = nullptr;
+    std::unique_ptr<tflite::Interpreter> m_interpreter = nullptr;
 };
 
 #endif // __COCO_DETECTION_FILTER__
